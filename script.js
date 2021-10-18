c6ffc0a783131709635fa41ed9b921cd976e39a0
BogdanovGennadievichRuslan <88258628+BogdanovGennadievichRuslan@users.noreply.github.com>
2021-10-18 16:47:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+closeButton.onclick = function myFunctionClose() {
+	var x = document.getElementById('menu');
+
+	x.className = "section-burger-menu-of"
+}
+
+burgerButton.onclick = function myFunctionON() {
+	var x = document.getElementById('menu');
+
+    x.className = "section-burger-menu";
+}
+
